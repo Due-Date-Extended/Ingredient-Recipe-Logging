b0a16e36198c9d29814201605b1a90556d7e7403
evil-tami <lolnonotyet@gmail.com>
2025-05-14 23:01:18 +0000

    Models Updated

---
+++ App/models/favorite.py
@@ -0,0 +1,20 @@
+from App.database import db
+from datetime import datetime, timezone
+
+class Favorite(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+  def __init__(self, user_id, recipe_id):
+      self.user_id = user_id
+      self.recipe_id = recipe_id
+      self.date_added = datetime.now(timezone.utc)
+      self.recipes = []
+
+  def get_json(self):
+    return{
+        'id': self.id,
+        'recipe_id': self.recipe_id,
+        'user_id': self.user_id
+    }
